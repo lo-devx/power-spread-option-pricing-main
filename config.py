f6c749eb695098fb139c22d051dbecb1efc5c9b9
lo-devx <lynodia@outlook.com>
2025-08-20 22:16:11 +0100

    kirk_approximation_model - complete

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 from datetime import datetime, timedelta
 
 #Define directories
-raw_data_dir = "/Users/Lyndon.Odia/Desktop/sede-test/power-spread-option-pricing/data/raw"
-processed_data_dir = "/Users/Lyndon.Odia/Desktop/sede-test/power-spread-option-pricing/data/processed"
+raw_data_dir = "/Users/Lyndon.Odia/Desktop/lo-devx/power-spread-option-pricing-main/data/raw"
+processed_data_dir = "/Users/Lyndon.Odia/Desktop/lo-devx/power-spread-option-pricing-main/data/processed"
 
 # Define time range - 7 months 
 START_DATE = datetime(2025, 1, 1, 0, 0)
@@ -17,3 +17,4 @@ API_KEY = "3faef1ee-b130-4678-9759-4ac9c0af0941"
 
 #Define bidding zone for FR
 FR_DOMAIN = "10YFR-RTE------C"
+
